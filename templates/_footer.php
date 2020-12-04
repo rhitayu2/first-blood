ffaab5a67b5f345d0c9d158bc26073330d6495c1
rhitayu2 <rhitayu2>
2020-12-04 13:24:29 +0530

    added icons

--- templates/_footer.php
+++ templates/_footer.php
@@ -1,7 +1,14 @@
-	<body>
-		<div class="footer">
-			<br><b>Made By:</b>
-			<br>Rhitayu Chattopadhyay
-		</div>
+<body>
+			<div class="footer" >
+				<br><b>Made By:</b>
+				<p>
+					Rhitayu Chattopadhyay&nbsp;&nbsp;
+					<a href="https://github.com/rhitayu2/first-blood">
+					<img src="/images/github.svg" style="width: 25px;height: 25px;"></a>
+					&nbsp;&nbsp;
+					<a href="https://www.linkedin.com/in/rhitayu/">
+					<img src="/images/linkedin.svg" style="width: 25px;height: 25px;">
+				</p>
+			</div>
 	</body>
 </html>
